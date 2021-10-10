00196a5b448748ea1d10a83fd1bb5043b8c90ffc
emrahbickingc <emrah.bicki@ngc.com.tr>
2021-10-10 20:51:58 +0300

    last update

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,8 @@ COPY requirements.txt /tmp
 WORKDIR /tmp
 RUN pip install --upgrade pip && \
     pip install -r requirements.txt
+RUN mkdir /app
+WORKDIR /app
+COPY app.py .
+ENTRYPOINT [ "python" ]
+CMD [ "app.py" ]
--- app.py
+++ app.py
@@ -7,33 +7,7 @@ application.debug = True
 
 @application.route('/')
 def hello_world():
-  storage = Storage()
-  storage.populate()
-  score = storage.score()
-  return "Hello Devops 123, %d!" % score
-
-class Storage():
-  def __init__(self):
-    self.db = MySQLdb.connect(
-      user   = os.getenv('db_user'),
-      passwd = os.getenv('Mytest123**'),
-      db     = os.getenv('test_db'),
-      host   = os.getenv('localhost'),
-      port   = int(os.getenv('3306'))
-    )
-
-    cur = self.db.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS scores(score INT)")
-
-  def populate(self):
-    cur = self.db.cursor()
-    cur.execute("INSERT INTO scores(score) VALUES(1234)")
-
-  def score(self):
-    cur = self.db.cursor()
-    cur.execute("SELECT * FROM scores")
-    row = cur.fetchone()
-    return row[0]
+  return "Hello Devops 123"
 
 if __name__ == "__main__":
   application.run(host='0.0.0.0', port=3000)
